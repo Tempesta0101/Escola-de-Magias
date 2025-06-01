68b3152c5832d443ad4bb02abe81cfebd6ce02a4
Tempesta0101 <juliano.tempesta@alunos.unis.edu.br>
2025-06-01 13:52:35 -0300

    1.0.1

--- app.php
+++ app.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use App\Model\Example;
+use Hogwarts\App;
 
-$test = new Example();
-echo $test->sayHello();
+$app = new App();
+$app->run();
---
+++ src/Model/app.php
@@ -0,0 +1,57 @@
+<?php
+namespace Hogwarts;
+
+use Hogwarts\Model\ConviteCadastro\ConviteManager;
+use Hogwarts\Model\SelecaoCasas\SelecaoCasasManager;
+use Hogwarts\Model\TorneiosCompeticoes\TorneiosManager;
+use Hogwarts\Model\ControleAcademico\ControleAcademicoManager;
+use Hogwarts\Model\GerenciamentoPessoal\ProfessoresManager;
+use Hogwarts\Model\AlertasComunicacao\AlertasManager;
+
+class App
+{
+    public function run()
+    {
+        echo "=== Sistema de Gestão Escolar Hogwarts ===\n";
+        echo "Selecione um módulo:\n";
+        echo "1 - Convite e Cadastro de Alunos\n";
+        echo "2 - Seleção de Casas\n";
+        echo "3 - Gerenciamento de Torneios e Competições\n";
+        echo "4 - Controle Acadêmico e Disciplinar\n";
+        echo "5 - Gerenciamento de Professores e Funcionários\n";
+        echo "6 - Sistema de Alertas e Comunicação\n";
+        echo "0 - Sair\n";
+        echo "Escolha: ";
+
+        $choice = trim(fgets(STDIN));
+
+        switch ($choice) {
+            case '1':
+                (new ConviteManager())->menu();
+                break;
+            case '2':
+                (new SelecaoCasasManager())->menu();
+                break;
+            case '3':
+                (new TorneiosManager())->menu();
+                break;
+            case '4':
+                (new ControleAcademicoManager())->menu();
+                break;
+            case '5':
+                (new ProfessoresManager())->menu();
+                break;
+            case '6':
+                (new AlertasManager())->menu();
+                break;
+            case '0':
+                echo "Saindo...\n";
+                exit;
+            default:
+                echo "Opção inválida.\n";
+                break;
+        }
+        // Re-executa o menu após ação
+        $this->run();
+    }
+}
